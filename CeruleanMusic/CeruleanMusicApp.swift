396c41c2704f8c211d08a1858951ab827d1c653b
Zelda110 <Zelda_110@icloud.com>
2025-10-12 12:04:27 +0800

    Initial Commit

---
+++ CeruleanMusic/CeruleanMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeruleanMusicApp.swift
+//  CeruleanMusic
+//
+//  Created by ハイラル・ゼルダ on 12/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct CeruleanMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
